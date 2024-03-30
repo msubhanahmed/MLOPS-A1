5c94a0dbb92c1144596fca0504e55206e1bdb3de
Muhammad Subhan <subhanahmed114624@gmail.com>
2024-03-30 12:38:41 +0500

    Refractored (#4)
    * Initial Backend
    
    * refactor code
    
    * Refactored
    
    * Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import joblib
 app = Flask(__name__)
 model = joblib.load('linear_regression_model.pkl')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
@@ -12,6 +13,7 @@ def predict():
     X = np.array(values).reshape(1, -1)
     prediction = model.predict(X)
     return jsonify({'predicted_price': prediction})
+
 
 if __name__ == '__main__':
     app.run(debug=False)
