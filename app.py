99dbdf763567cdb06fb4f746847083beada94702
Muhammad Subhan <subhanahmed114624@gmail.com>
2024-03-30 12:34:52 +0500

    Refractored Code (#3)
    * Initial Backend
    
    * refactor code
    
    * Refactored

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from flask import Flask, request, jsonify
 import numpy as np
 import joblib
 
-app   = Flask(__name__)
+app = Flask(__name__)
 model = joblib.load('linear_regression_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-
-    data        = request.json
-    values      = [data['plot_size']]
-    X           = np.array(values).reshape(1, -1)
-    prediction  = model.predict(X)
+    data = request.json
+    values = [data['plot_size']]
+    X = np.array(values).reshape(1, -1)
+    prediction = model.predict(X)
     return jsonify({'predicted_price': prediction})
 
 if __name__ == '__main__':
